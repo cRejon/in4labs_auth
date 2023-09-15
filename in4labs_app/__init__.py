0fadefc24e21e6504e2700d9550cf45ce373fc02
cRejon <carlosrejongomez@gmail.com>
2023-09-15 13:02:15 +0200

    Fix docker API functionality

--- in4labs_app/__init__.py
+++ in4labs_app/__init__.py
@@ -58,26 +58,25 @@ for lab in labs:
         with open(html_dest, 'w') as f:
             f.write(html_content)
 
-try:   # for development purposes without docker
-    # Create docker image for each lab if not exists
-    client = docker.from_env()
-    for lab in labs:
-        lab_name = lab['lab_name']
-        image_name = f'{lab_name.lower()}:latest'
-        try:
-            client.images.get(image_name)
-        except docker.errors.ImageNotFound:
-            print(f'Creating Docker image {image_name}.')
-            dockerfile_path = os.path.join(basedir, 'labs', lab_name)
-            image, build_logs = client.images.build(
-                path=dockerfile_path,
-                tag=image_name,
-                rm=True,
-            )
-            for log in build_logs: # Print the build logs for debugging purposes
-                print(log.get('stream', '').strip())
-except:
-    pass
+
+# Create docker image for each lab if not exists
+client = docker.from_env()
+for lab in labs:
+    lab_name = lab['lab_name']
+    image_name = f'{lab_name.lower()}:latest'
+    try:
+        client.images.get(image_name)
+    except docker.errors.ImageNotFound:
+        print(f'Creating Docker image {image_name}. Be patient, this will take a while...')
+        dockerfile_path = os.path.join(basedir, 'labs', lab_name)
+        image, build_logs = client.images.build(
+            path=dockerfile_path,
+            tag=image_name,
+            rm=True,
+        )
+        for log in build_logs: # Print the build logs for debugging purposes
+            print(log.get('stream', '').strip())
+        print(f'Docker image {image_name} created successfully.')
 
 # Create a hashed password for the Jupyter notebook
 def create_hash(password):
@@ -182,8 +181,9 @@ def enter_lab(lab_name):
             'CAM_URL': lab.get('cam_url', ''),
             'NOTEBOOK_PASSWORD': notebook_password,
         }
+        image_name = f'{lab_name.lower()}:latest'
         port = lab['host_port']
-        container = client.containers.run(f'{lab_name}:latest', detach=True, remove=True,
+        container = client.containers.run(image_name, detach=True, remove=True,
                                             ports={'8000/tcp': ('0.0.0.0', port)}, environment=docker_env)
 
         remaining_secs = (end_time - datetime.now()).total_seconds()
